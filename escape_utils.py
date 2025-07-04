44e3294f5246d44b344959480e30dafed053a8ce
riya-235 <riyarpatel08@gmail.com>
2025-07-04 12:22:37 -0700

    Add files via upload

---
+++ escape_utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  3 15:55:02 2025
+
+@author: riya
+"""
+
+# escape_utils.py
+
+def reveal_key_1():
+    print("✅ Puzzle 1 Solved! Key: 739")
+
+def reveal_key_2():
+    print("✅ Puzzle 2 Solved! Key: 54")
+
+def reveal_key_3():
+    print("✅ Puzzle 3 Solved! Key: 67")
+
+def reveal_key_4():
+    print("✅ Puzzle 4 Solved! Key: 1078")
+
+def reveal_key_5():
+    print("✅ Puzzle 5 Solved! Key: 555")
